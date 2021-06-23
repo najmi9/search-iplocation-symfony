edc16cb2dec996c4141f5167826cc0fa8ae7cfc0
najmi9 <imadnajmi9@gmail.com>
2021-06-23 23:11:35 +0100

    clean code

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,11 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Yaml\Yaml;
 
 class HomeController extends AbstractController
 {
